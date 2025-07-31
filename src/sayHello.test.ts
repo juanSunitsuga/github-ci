9fe52241bf13480c11dd0ac6deb5f2eb59dabaf2
juanSunitsuga <juankurniawan04@gmail.com>
2025-07-31 09:30:13 +0700

    fixed test

--- src/sayHello.test.ts
+++ src/sayHello.test.ts
@@ -2,7 +2,7 @@ import {sayHello} from './sayHello';
 
 describe('sayHello', () => {
     it('should return a string with the name', () => {
-        expect(sayHello('John')).toBe('Hell, John!');
+        expect(sayHello('John')).toBe('Hello, John!');
     });
 })
 
