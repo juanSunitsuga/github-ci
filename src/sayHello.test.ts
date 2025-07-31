bef34a46c27d5b6f782a73f7a0225f4d02c386c5
juanSunitsuga <juankurniawan04@gmail.com>
2025-07-31 09:18:26 +0700

    init commit

---
+++ src/sayHello.test.ts
@@ -0,0 +1,7 @@
+import {sayHello} from './sayHello';
+
+describe('sayHello', () => {
+    it('should return a string with the name', () => {
+        expect(sayHello('John')).toBe('Hello, John!');
+    });
+})
